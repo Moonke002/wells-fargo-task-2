860c090af2f2f8c47601215de3e110e56a080fd2
Moonke002 <baubs270@gmail.com>
2025-12-02 12:15:08 -0500

    final

---
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -0,0 +1,53 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "Portfolios")
+
+public class Portfolio {
+
+    @Id
+    @GeneratedValue()
+    private Long portfolioId;
+
+    @OneToOne
+    @JoinColumn(name = "clientId", nullable = false)
+    private Client client;
+
+    @Column(nullable = false)
+    private LocalDate creationDate;
+
+    protected Portfolio() {
+    }
+
+    public Portfolio(Client client, LocalDate creationDate) {
+        this.client = client;
+        this.creationDate = creationDate;
+    }
+
+    public Long getPortfolioId() {
+        return portfolioId;
+
+    }
+
+
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+}
